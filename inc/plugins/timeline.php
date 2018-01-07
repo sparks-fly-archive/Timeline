d23e966f82125ab7d3c62f102fe8283888ce8fdc
Jule <34949343+sparks-fly-archive@users.noreply.github.com>
2018-01-07 19:17:39 +0100

    Events hinzufügen

---
+++ inc/plugins/timeline.php
@@ -0,0 +1,84 @@
+<?php
+
+// Disallow direct access to this file for security reasons
+if(!defined("IN_MYBB"))
+{
+	die("Direct initialization of this file is not allowed.");
+}
+
+
+function timeline_info()
+{
+	return array(
+		"name"			=> "Stadtgedächtnis",
+		"description"	=> "Erstellt Tabellen, Templates und Verknüpfungen für die <em>Stadtgedächtnis</em>-Erweiterung.",
+		"website"		=> "http://dirty-paws.de",
+		"author"		=> "sparks fly",
+		"authorsite"	=> "http://github.com/user/its-sparks-fly",
+		"version"		=> "1.0",
+		"compatibility" => "*"
+	);
+}
+
+function timeline_install()
+{
+	global $db;
+
+	if(!$db->table_exists("timeline")) {
+		$db->query("CREATE TABLE `mybb_timeline` (
+  				`eid` int(11) NOT NULL AUTO_INCREMENT,
+  				`uid` int(11) NOT NULL,
+  				`date` varchar(20) NOT NULL,
+  				`title` text NOT NULL,
+  				`description` text NOT NULL,
+  				`tagged` text NOT NULL,
+  				`accepted` int(1) NOT NULL,
+  				PRIMARY KEY (`eid`)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;");
+	}
+
+	if(!$db->table_exists("timeline_users")) {
+		$db->query("CREATE TABLE `mybb_timeline_users` (
+			  	`eid` int(11) NOT NULL AUTO_INCREMENT,
+  				`uid` int(11) NOT NULL,
+  				`tid` int(11) NOT NULL,
+  				`description` text NOT NULL,
+  				PRIMARY KEY (`eid`)
+  			) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;");
+	}
+}
+
+function timeline_is_installed()
+{
+    global $db;
+
+    if($db->table_exists("timeline")) {
+        return true;
+    }
+    return false;
+}
+
+function timeline_uninstall()
+{
+	global $db;
+
+	if($db->table_exists("timeline")) {
+		$db->query("DROP TABLE `mybb_timeline`");
+	}
+
+	if($db->table_exists("timeline_users")) {
+		$db->query("DROP TABLE `mybb_timeline_users`");
+	}
+}
+
+function timeline_activate()
+{
+
+}
+
+function timeline_deactivate()
+{
+
+}
+
+?>
--- timeline.php
+++ timeline.php
@@ -1,45 +1,99 @@
 <?php
 
-// Set some useful constants that the core may require or use
+// set some useful constants that the core may require or use
 define("IN_MYBB", 1);
 define('THIS_SCRIPT', 'timeline.php');
 
-// Including global.php gives us access to a bunch of MyBB functions and variables
+// including global.php gives us access to a bunch of MyBB functions and variables
 require_once "./global.php";
 
-// Load Language-Settings
+// load language-settings
 $lang->load('timeline');
 
-// Shorten variables
+// shorten variables
 $action = $mybb->input['action'];
 $uid = $mybb->user['uid'];
 
-// Add a breadcrumb
+// add a breadcrumb
 add_breadcrumb('Stadtgeschichte', "timeline.php");
 
-// Set navigation
+// set navigation
 eval("\$timeline_nav = \"".$templates->get("timeline_navigation")."\";");
 
-// Set navigation so only team members can see certain options
+// set navigation so only team members can see certain options
 $timeline_nav_team = "";
 if($mybb->usergroup['cancp'] == "1") {
 	eval("\$timeline_nav_team = \"".$templates->get("timeline_navigation_team")."\";");	
 }
 
-// Landing Page
+// landing page
 if(empty($action)) {
 	
-	// Set Landing Page-Template
+	// set landing page-template
 	eval("\$page = \"".$templates->get("timeline")."\";");
 	output_page($page);
 }
 
-// FAQ-Page
+// FAQ-page
 if($action == "faq") {
 	
-	// Set FAQ Page-Template
+	// set FAQ-page-template
 	eval("\$page = \"".$templates->get("timeline_faq")."\";");
 	output_page($page);
+}
+
+// add event-page
+if($action == "add") {
+
+	// format date dropdowns
+	for($i=1 ; $i <=31; $i++) {
+    	$day_bit .= "<option value=\"{$i}\">{$i}</option>";
+  	}
+
+  	$months = array("01" => "Januar", "02" => "Februar", "03" => "März", "04" => "April", "05" => "Mai", "06" => "Juni", "07" => "Juli", "08" => "August", "09" => "September", "10" => "Oktober", "11" => "November", "12" => "Dezember");
+  	foreach($months as $key => $value) {
+    	$month_bit .= "<option value=\"{$key}\">{$value}</option>";
+  	}
+
+ 	for($i=1970 ; $i <=2017; $i++) {
+    	$year_bit .= "<option value=\"{$i}\">{$i}</option>";
+  	}
+
+	// set add event-page-template
+	eval("\$page = \"".$templates->get("timeline_add")."\";");
+	output_page($page);
+}
+
+// add event backend
+if($action == "do_add") {
+
+	$members_new = explode(",", $mybb->get_input('members'));
+	$members_new = array_map("trim", $members_new);
+	$members_uids = array();
+	foreach($members_new as $member) {
+		$db->escape_string($member);
+		$member_uid = $db->fetch_field($db->query("SELECT uid FROM ".TABLE_PREFIX."users WHERE username = '$member'"), "uid");
+		$members_uids[] = $member_uid;
+	}
+	$members_uids = implode(",", $members_uids);
+
+	$ipdate = strtotime($mybb->get_input('year')."-".$mybb->get_input('month')."-".$mybb->get_input('day'));
+
+	// data to insert into database
+	$new_record = array(
+		"title" => $db->escape_string($mybb->get_input('name')),
+		"date" => $ipdate,
+		"description" => $db->escape_string($mybb->get_input('desc')),
+		"tagged" => $members_uids,
+		"uid" => (int)$uid,
+		"accepted" => (int)"1"
+	);
+
+	// insert entry
+	$db->insert_query("timeline", $new_record);
+
+	// stuff is done, redirect to landing page
+	redirect("timeline.php", "{$lang->timeline_added}");
 }
 
 ?>
